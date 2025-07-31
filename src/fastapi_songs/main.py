fc3f8fa66a3df2b4b25d95cac7de30e4d7030a01
raylai <chunjueilai@gmail.com>
2025-07-31 22:27:25 +0800

    feat: add fastapi entry:root and launch feat:main

---
+++ src/fastapi_songs/main.py
@@ -0,0 +1,23 @@
+"""
+Summary: main.py is FastAPI entry with all pages.
+
+Name: Chun-Juei Lai
+Email: chunjueilai@gmail.com
+Date: 07/31/2025
+"""
+
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    """Fastapi website root entry."""
+    return {"name": "FastAPI_songs"}
+
+
+def main():
+    """Launch 'poetry run fastapi_songs' at root."""
+    uvicorn.run("fastapi_songs.main:app", host="127.0.0.1", port=8000, reload=True)
