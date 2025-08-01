8458bbcf1d3c620c2c286bc2e3d3b5c7c28175e2
raylai <chunjueilai@gmail.com>
2025-08-01 20:48:03 +0800

    fix: add reference

--- src/fastapi_songs/database.py
+++ src/fastapi_songs/database.py
@@ -1,6 +1,7 @@
 """
 Summary: database.py declare engine, session, and declarative base for sqlalchemy to use.
 
+Reference: https://github.com/duplxey/fastapi-songs/blob/master/database.py
 Author: Chun-Juei Lai
 Email: chunjueilai@gmail.com
 Date: 08/01/2025
