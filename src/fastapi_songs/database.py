c98dfdde46337bb4729b3a746fedf4d62f6753ac
raylai <chunjueilai@gmail.com>
2025-08-01 20:44:45 +0800

    feat: create engine, session, table base

--- src/fastapi_songs/database.py
+++ src/fastapi_songs/database.py
@@ -5,3 +5,13 @@ Author: Chun-Juei Lai
 Email: chunjueilai@gmail.com
 Date: 08/01/2025
 """
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./default.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+session_local = sessionmaker(bind=engine, autoflush=False)
+Base = declarative_base()
