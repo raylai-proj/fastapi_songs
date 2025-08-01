e2299034b49a8dadbc54beb9eab27142cf29bec0
raylai <chunjueilai@gmail.com>
2025-08-01 21:44:25 +0800

    feat: add init_db.py

---
+++ src/fastapi_songs/init_db.py
@@ -0,0 +1,8 @@
+"""
+Summary: initialize database when first time launch fastapi.
+
+Reference: https://github.com/duplxey/fastapi-songs/blob/master/init_db.py
+Author: Chun-Juei Lai
+Email: chunjueilai@gmail.com
+Date: 08/01/2025
+"""
