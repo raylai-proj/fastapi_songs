9c05f547b9ae6128d385045c00785beed562cbee
raylai <chunjueilai@gmail.com>
2025-08-01 20:58:40 +0800

    feat: add Song class

--- src/fastapi_songs/models.py
+++ src/fastapi_songs/models.py
@@ -6,3 +6,16 @@ Author: Chun-Juei Lai
 Email: chunjueilai@gmail.com
 Date: 08/01/2025
 """
+
+from database import Base
+from sqlalchemy import Column, Date, Integer, String
+
+
+class Song(Base):
+    """Define Song table inherit declarative_base."""
+
+    __tablename__ = "songs"
+    id = Column(type_=Integer, primary_key=True)
+    title = Column(type_=String(128), nullable=False)
+    artist = Column(type_=String(128), nullable=False)
+    release_date = Column(type_=Date, nullable=False)
