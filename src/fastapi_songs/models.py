093257066573bb02a768264a4f8759e701431a1a
raylai <chunjueilai@gmail.com>
2025-08-01 20:57:10 +0800

    fix: add reference

--- src/fastapi_songs/models.py
+++ src/fastapi_songs/models.py
@@ -1,6 +1,7 @@
 """
 Summary: models.py defines Song table inherit from Base.
 
+Reference: https://github.com/duplxey/fastapi-songs/blob/master/models.py
 Author: Chun-Juei Lai
 Email: chunjueilai@gmail.com
 Date: 08/01/2025
