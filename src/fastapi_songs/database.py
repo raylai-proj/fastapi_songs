67057bd5e22d0784e6938be797c0c697ef8d30b6
raylai <chunjueilai@gmail.com>
2025-08-05 17:24:59 +0800

    fix: fix database location

--- src/fastapi_songs/database.py
+++ src/fastapi_songs/database.py
@@ -13,7 +13,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DB_PATH = os.path.join(BASE_DIR, "default.db")
 SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
